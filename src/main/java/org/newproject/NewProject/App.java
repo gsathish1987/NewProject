ac8aab3b7ce070bc05d96ab76a7a751959eaff79
gsathish1987 <gsathish.k1987@gmail.com>
2022-04-19 10:30:47 +0530

    Project Details

---
+++ src/main/java/org/newproject/NewProject/App.java
@@ -0,0 +1,13 @@
+package org.newproject.NewProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
